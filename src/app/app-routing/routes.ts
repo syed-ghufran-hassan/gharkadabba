f91c78356e05e1bee721c6d97433ef2de47e6f65
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2021-01-03 22:30:58 +0500

    Resolved menu component html issue

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-
 import { MenuComponent } from '../menu/menu.component';
 import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 import { HomeComponent } from '../home/home.component';
@@ -7,9 +6,11 @@ import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 
 
+
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
   { path: 'contactus',     component: ContactComponent },
+  { path: 'dishdetail/:id',     component: DishdetailComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
